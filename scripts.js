df722212955268985a6c9e3f175cd889f9a0e08d
Erik Irgens and Na Hyung Choi <na@>
2019-09-16 13:54:42 -0700

    made RESTART_SONG action

--- scripts.js
+++ scripts.js
@@ -4,6 +4,23 @@ const songLyricsArray = "Don't want to be a fool for you, Just another player in
 
 // INITIAL REDUX STATE
 const initialState = {
+  currentSongId: null,
+  songsById: {
+    1: {
+      title: "Bye Bye Bye",
+      artist: "N'Sync",
+      songId: 1,
+      songArray: songList[1],
+      arrayPosition: 0,
+    },
+    2: {
+      title: "What's Goin' On",
+      artist: "Four Non-Blondes",
+      songId: 2,
+      songArray: songList[2],
+      arrayPosition: 0,
+    },
+  }
   songLyricsArray: songLyricsArray,
   arrayPosition: 0,
 }
@@ -17,6 +34,9 @@ const reducer = (state = initialState, action) => {
         songLyricsArray: state.songLyricsArray,
         arrayPosition: newArrayPosition,
       }
+      return newState;
+    case 'RESTART_SONG':
+      newState = initialState;
       return newState;
     default:
       return state;
@@ -34,12 +54,43 @@ expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
   arrayPosition: 1
 });
 
+expect(reducer({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
+},
+  { type: 'RESTART_SONG' })
+).toEqual(initialState);
+
 //REDUX STORE
 const { createStore } = Redux;
 const store = createStore(reducer);
-console.log(store.getState());
+
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
+window.onload = function () {
+  renderLyrics();
+}
 
 // CLICK LISTENER
 const userClick = () => {
+  const currentState = store.getState();
+  if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
+    store.dispatch({ type: 'RESTART_SONG' });
+  } else {
+    store.dispatch({ type: 'NEXT_LYRIC' });
+  }
   console.log('click');
+  store.dispatch({ type: 'NEXT_LYRIC' });
 }
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
